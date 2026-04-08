76d329de6af7717d5e555a838027467e7620cda8
wangtianyu <wangtianyu24@mails.ucas.ac.cn>
2026-04-08 12:50:02 +0800

    first commit

--- example.py
+++ example.py
@@ -1,13 +1,19 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
+
+device = "cuda:0"
 
 def main():
+    model_path = os.path.expanduser("~/huggingface/Qwen3-0.6B/")
+    
     msg = "Hello, my name is "
-    tokenizer = AutoTokenizer.from_pretrained("~/huggingface/Qwen3-0.6B/")
-    model = AutoModelForCausalLM.from_pretrained("~/huggingface/Qwen3-0.6B/", 
-                                                 device_map="auto")
-    inputs = tokenizer(msg, return_tensors="pt")
-    output = model.generate(input_ids=inputs["input_ids"], max_length=1000)
-    print(output)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path, 
+                                                 device_map=device)
+    inputs = tokenizer(msg, return_tensors="pt").to(device)
+    output = model.generate(input_ids=inputs["input_ids"], max_length=1000,temperature=2.0, do_sample=True)
+    print(tokenizer.decode(inputs["input_ids"], skip_special_tokens=True))
+    print(tokenizer.decode(output[0][len(inputs["input_ids"][0]):], skip_special_tokens=True))
 
 if __name__ == "__main__":
     main()
