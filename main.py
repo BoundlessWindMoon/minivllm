c86490cb5b8f3c67a8201198efea551c0c8510da
wangtianyu <wangtianyu24@mails.ucas.ac.cn>
2026-04-09 15:12:18 +0800

    split create model and inject data

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import os
 import torch
+from glob import glob
+from torch import nn
+from safetensors import safe_open
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 
 device = "cuda:0"
 max_new_tokens = 128
+
+def default_weight_loader(param: nn.Parameter, loaded_weight: torch.Tensor):
+    param.data.copy_(loaded_weight)
+
+def load_model(model: nn.Module, path: str):
+    packed_modules_mapping = getattr(model, "packed_modules_mapping", {})
+    for file in glob(os.path.join(path, "*.safetensors")):
+        with safe_open(file, "pt", "cpu") as f:
+            for weight_name in f.keys():
+                for k in packed_modules_mapping:
+                    if k in weight_name:
+                        v, shard_id = packed_modules_mapping[k]
+                        param_name = weight_name.replace(k, v)
+                        param = model.get_parameter(param_name)
+                        weight_loader = getattr(param, "weight_loader")
+                        weight_loader(param, f.get_tensor(weight_name), shard_id)
+                        break
+                else:
+                    param = model.get_parameter(weight_name)
+                    weight_loader = getattr(param, "weight_loader", default_weight_loader)
+                    weight_loader(param, f.get_tensor(weight_name))
 
 def compute_logits(model, input_ids: torch.Tensor, position: torch.Tensor, is_prefill: bool) -> torch.Tensor:
     outputs = model(
@@ -25,9 +49,14 @@ def main():
     model_path = os.path.expanduser("~/huggingface/Qwen3-0.6B/")
     
     msg = "Hello, my name is "
+    
     tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForCausalLM.from_pretrained(model_path, 
-                                                 device_map=device)
+    config = AutoConfig.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path, config=config).to(device)
+    load_model(model, model_path)
+    
+    print(model)
+    
     inputs = tokenizer(msg, return_tensors="pt").to(device)
     input_ids = inputs["input_ids"].to(device)
     position_ids = torch.arange(input_ids.shape[1], device=device).unsqueeze(0)
